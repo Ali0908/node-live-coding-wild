cf2eeed82a7b42e7fc0771868deb8cb7d92d00ea
Ali <zaabat97@gmail.com>
2023-04-25 11:19:37 +0200

    création d'un WilderController

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const typeorm = require("typeorm")
-const app = express();
-const Wilder = require ("./entity/Wilder");
 
-const dataSource = new typeorm.DataSource({
-  type: "sqlite",
-  database: ".wildersdbs.sqlite",
-  synchronize: true,
-  entities: [require("./entity/Wilder")],
-  logging :["query","error"]
-});
+const dataSource = require ("./utils").dataSource;
+const wilderController = require ("./controller/wilder");
+
+const app = express();
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.post ("/api/wilder", wilderController.create);
 const start = async () => {
   await dataSource.initialize();
-  dataSource.getRepository(Wilder).save({name: "First Wilder"});
+  // dataSource.getRepository(Wilder).save({name: "First Wilder"});
   app.listen(3000, () => console.log("Server started on 3000"));
 };
 //Start Server
